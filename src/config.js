bdf75a082258bae3d60aa28908e9c01440ae14f8
Dendi <352671309@qq.com>
2020-11-30 21:28:55 +0800

    ...

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ module.exports = {
   code: 'koa2-test-config', // 生产token的密码
   // uri: 'mongodb+srv://Dendi:WZf@015153850@cluster0.ye5ci.mongodb.net/Cluster0?retryWrites=true&w=majority'
   uri: 'mongodb://localhost:27017/users'
-
 }
 
